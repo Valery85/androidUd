6bbf5318b804dc66038297ce8e65cea41b2dfa48
v <mr.valery85@yandex.ru>
2018-05-21 15:59:25 -0800

    Add methods to set fixed score when button tapped

--- BallCounter/app/src/main/java/com/examlpe/android/ballcounter/MainActivity.java
+++ BallCounter/app/src/main/java/com/examlpe/android/ballcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.examlpe.android.ballcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addThreeToA(View view){
+        displayScoreTeamA(3);
+    }
+    public void addTwoToA(View view){
+        displayScoreTeamA(2);
+    }
+    public void addOneToA(View view){
+        displayScoreTeamA(1);
+    }
+
+    private void displayScoreTeamA(int number){
+        TextView teamAScore = (TextView) findViewById(R.id.team_a_score);
+        teamAScore.setText(String.valueOf(number));
+
     }
 }
