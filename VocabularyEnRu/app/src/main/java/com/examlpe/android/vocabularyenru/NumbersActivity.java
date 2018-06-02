fac86412d7a2a5001052e6458e23edd607f4a92a
v <mr.valery85@yandex.ru>
2018-06-01 16:48:48 -0800

    Make right naming, delete unneeded files

--- VocabularyEnRu/app/src/main/java/com/examlpe/android/vocabularyenru/NumbersActivity.java
+++ VocabularyEnRu/app/src/main/java/com/examlpe/android/vocabularyenru/NumbersActivity.java
@@ -17,11 +17,7 @@ package com.examlpe.android.vocabularyenru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -32,7 +28,7 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
 /*        // Create array of words (numbers)
         String [] words = new String [10];
