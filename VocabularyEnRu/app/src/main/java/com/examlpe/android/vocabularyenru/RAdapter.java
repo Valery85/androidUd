999defe953f1e320f56eb426d9f19ad6291f19ed
Rik <mr.valery85@yandex.ru>
2018-06-10 17:09:18 +0300

    Show image for all categories except PhrasesActivity (bug will be fixed later)

--- VocabularyEnRu/app/src/main/java/com/examlpe/android/vocabularyenru/RAdapter.java
+++ VocabularyEnRu/app/src/main/java/com/examlpe/android/vocabularyenru/RAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -70,6 +71,9 @@ public class RAdapter extends ArrayAdapter<Word> {
 
         TextView enWordTV = (TextView) listItemView.findViewById(R.id.enWordTV);
         enWordTV.setText(currentWord.getmEnWord());
+
+        ImageView wordImageView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+        wordImageView.setImageResource(currentWord.getmImageResourceId());
 
         return listItemView;
     }
