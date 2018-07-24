f091e8ae3fe9a47019d6e03e1b51a09c03044cb4
v <mr.valery85@yandex.ru>
2018-07-24 17:06:03 +0300

    Add tabs with correspondent names of categories

--- VocabularyEnRu/app/src/main/java/com/examlpe/android/vocabularyenru/CategoryAdapter.java
+++ VocabularyEnRu/app/src/main/java/com/examlpe/android/vocabularyenru/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package com.examlpe.android.vocabularyenru;
 
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -11,12 +12,13 @@ import android.support.v4.app.FragmentPagerAdapter;
  * each list item based on a data source which is a list of {@link Word} objects.
  */
 public class CategoryAdapter extends FragmentPagerAdapter {
+    /** Context of the app */
+    private Context mContext;
 
-    public CategoryAdapter (FragmentManager fm) {
+    public CategoryAdapter(Context context, FragmentManager fm) {
         super(fm);
-        }
-        /**Return {@link Fragment} that will be shown for the given page number
-         * */
+        mContext = context;
+    }
     @Override
     public Fragment getItem(int position) {
         if (position == 0) {
@@ -36,5 +38,18 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
     }
 }
