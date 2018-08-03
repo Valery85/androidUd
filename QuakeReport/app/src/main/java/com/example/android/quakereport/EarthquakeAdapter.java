2f736c9a8eb9832cdf9fb4727f8fb9f3ee248672
v <mr.valery85@yandex.ru>
2018-08-03 00:49:47 -0800

    Store date and time separately in human readable format

--- QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -9,7 +9,9 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by rik on 8/1/2018.
@@ -38,9 +40,39 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         TextView locationTV = (TextView) listItemView.findViewById(R.id.location);
         locationTV.setText( " " + currentEarthquake.getLocation());
 
+        // Create Date object from the time in ms (when earthquake happened)
+        Date dateObject = new Date(currentEarthquake.getTimeInMilliseconds());
+
+        // date in human readable format (Jan 10, 2016)
+        String formattedDate = formatDate(dateObject);
+
         TextView dateTV = (TextView) listItemView.findViewById(R.id.date);
-        dateTV.setText( " " + currentEarthquake.getTimeInMilliseconds());
+        dateTV.setText(formattedDate);
+
+        // time in human readable format (7:10 pm)
+        String formattedTime = formatTime(dateObject);
+
+        TextView timeTV = (TextView) listItemView.findViewById(R.id.time);
+        timeTV.setText(formattedTime);
+
 
         return listItemView;
     }
+    /**
+     * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
+     */
+    private String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return dateFormat.format(dateObject);
+    }
+
+    /**
+     * Return the formatted date string (i.e. "4:30 PM") from a Date object.
+     */
+    private String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        return timeFormat.format(dateObject);
+    }
+
+
 }
