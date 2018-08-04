5eed9748e307ebc84f9c162e3200b20a7493545b
Rik <mr.valery85@yandex.ru>
2018-08-04 11:27:45 +0300

    Split location string on prime location and offset and show them in different TextView on the screen

--- QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.quakereport;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,25 +21,35 @@ import java.util.Date;
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
     public EarthquakeAdapter(@NonNull Context context, ArrayList<Earthquake> earthquakes) {
-        super(context,0, earthquakes);
+        super(context, 0, earthquakes);
+//        getLocationData("215km SW of Tomatlan, Mexico");
+//        getLocationOffset("215km SW of Tomatlan, Mexico");
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-       View listItemView = convertView;
-       if(listItemView == null){
-           listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_view, parent, false);
-       }
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_view, parent, false);
+        }
 
-       //Get the object that stored in current position in the list
-       Earthquake currentEarthquake = getItem(position);
+        //Get the object that stored in current position in the list
+        Earthquake currentEarthquake = getItem(position);
 
         TextView magnitudeTV = (TextView) listItemView.findViewById(R.id.magnitude);
-        magnitudeTV.setText( " " + currentEarthquake.getMagnitude());
+        magnitudeTV.setText(" " + currentEarthquake.getMagnitude());
 
-        TextView locationTV = (TextView) listItemView.findViewById(R.id.location);
-        locationTV.setText( " " + currentEarthquake.getLocation());
+//        TextView locationTV = (TextView) listItemView.findViewById(R.id.location);
+//        locationTV.setText( " " + currentEarthquake.getLocation());
+
+        TextView locationPrimaryTV = (TextView) listItemView.findViewById(R.id.location_primary);
+        String locationPrimary = getLocationData(currentEarthquake.getLocation());
+        locationPrimaryTV.setText(locationPrimary);
+
+        TextView locationOffsetTV = (TextView) listItemView.findViewById(R.id.location_offset);
+        String locationOffset = getLocationOffset(currentEarthquake.getLocation());
+        locationOffsetTV.setText(locationOffset);
 
         // Create Date object from the time in ms (when earthquake happened)
         Date dateObject = new Date(currentEarthquake.getTimeInMilliseconds());
@@ -58,6 +69,7 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
         return listItemView;
     }
+
     /**
      * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
      */
@@ -74,5 +86,33 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         return timeFormat.format(dateObject);
     }
 
+    // To get primary location by splitting string
+    private String getLocationData(String location) {
+        String locationData[] = location.split("of ", 2);
 
+//        Log.e("!!!!!EarthquakeAdapter", " : " +  locationData[0] + " of " + "\n" +
+//        locationData[1]);
+
+        // Not all raw strings with location have "of " pattern, this caused exceptions
+        //because created array contains of 1 element (whole initial string)
+        if (locationData.length > 1) {
+            return locationData[0] + "of ";
+        } else return "Near to";
+    }
+
+    private String getLocationOffset(String location) {
+        String locationData[] = location.split("of ", 2);
+//        Log.e("!!!!!EarthquakeAdapter", " : " +  locationData[0] + " of " + "\n" +
+//                locationData[1]);
+
+        // Not all raw strings with  location have "of " pattern, this caused exceptions
+        //because created array contains of  1 element (whole initial string) so in primary
+        // location I return "Near to" , and here return whole raw string which is zero element
+        //in array
+        if (locationData.length > 1) {
+            return locationData[1];
+        } else return locationData[0];
+
+
+    }
 }
